c3f0c43d60a3f69fd57d78bf91ca64394d4dfad8
Sir_Milllion <73513663+Sir-Million@users.noreply.github.com>
2024-07-07 02:19:48 -0600

    uy

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 // Reemplaza 'your-glitch-project-name' con el nombre de tu proyecto en Glitch
-const socket = io('https://nocardgame.glitch.me/');
+const socket = io('https://nocardgame.glitch.me');
 
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('actionButton').addEventListener('click', () => {
